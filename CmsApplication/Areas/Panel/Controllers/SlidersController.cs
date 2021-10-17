628e4fb9076fbbf5b818d42fec1f52847c054f88
berkyagmurlu <berk.yagmurluu@gmail.com>
2021-10-17 18:25:47 +0300

    File Upload

--- CmsApplication/Areas/Panel/Controllers/SlidersController.cs
+++ CmsApplication/Areas/Panel/Controllers/SlidersController.cs
@@ -1,6 +1,7 @@
 ﻿using CmsApplication.Areas.Panel.Models;
 using CmsApplication.Data;
 using Core.Flash;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,13 +16,17 @@ namespace CmsApplication.Areas.Panel.Controllers
     [Area("Panel")]
     public class SlidersController : Controller
     {
+        private string[] allowedExtensions = { ".jpg", ".jpeg", ".png" };
+
         private readonly ApplicationDbContext _context;
         private readonly IFlasher _f;
+        private IWebHostEnvironment _webHostEnvironment;
 
-        public SlidersController(ApplicationDbContext context, IFlasher flahser)
+        public SlidersController(ApplicationDbContext context, IFlasher flahser, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
             _f = flahser;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public async Task<IActionResult> Index()
@@ -38,14 +43,43 @@ namespace CmsApplication.Areas.Panel.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Slider slider, IFormFile sliderFile)
         {
-            if (ModelState.IsValid)
+            if (sliderFile.Length > 0)
             {
+                // Generate File Name 
+                // GUID
+                // Path.GetRandomFileName (Without Extension)
+
+                var extension = Path.GetExtension(sliderFile.FileName).ToLowerInvariant();
+                if (!this.allowedExtensions.Contains(extension))
+                {
+                    _f.Flash(Types.Danger, "Lütfen uygun dosya formatını seçiniz.");
+                    return View();
+                }
+                
+                string fileName = Guid.NewGuid().ToString() + extension;
+
+                // Create Upload Directory
+                var path = Path.Combine(this._webHostEnvironment.WebRootPath, "uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                // New File Stream Save File As Path.Combine
+                var stream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
+                sliderFile.CopyToAsync(stream);
+
+                slider.FileName = fileName;
                 slider.CreateAt = DateTime.Now;
                 _context.Add(slider);
                 await _context.SaveChangesAsync();
 
                 _f.Flash(Types.Success, "İşlem Başarıyla Gerçekleştirildi");
                 return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                _f.Flash(Types.Danger, "Lütfen dosya seçimi yapınız.");
             }
 
             return View(slider);
